e05e7178cfbdfea936c8708f9d56916fb338afd8
Alex <axell2479@gmail.com>
2026-06-11 22:55:30 +0000

    Перешит новый coin.mp3 в base64, bump SW кэша v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mm-v3";
+const CACHE = "mm-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./coin.mp3"];
 
 self.addEventListener("install", e => {
